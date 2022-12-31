dbf72c686669d62a3ee9f6bb0695f6500e66e7e4
technologies4you <114349783+web-software@users.noreply.github.com>
2022-12-31 15:49:19 -0600

    added activities controller

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -27,4 +27,21 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Run latest EF migrations
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+// --------------------------
+
 app.Run();
